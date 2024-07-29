8c56a6f05491b0b4db4cf4441b5f6cb1ff7271d4
mobin <mbynatrzade@gmail.com>
2024-07-29 20:27:37 +0430

    added send otp form components

---
+++ freelancer-app-frontend/src/pages/Auth.jsx
@@ -0,0 +1,9 @@
+import SendOTPForm from "../features/authentication/SendOTPForm";
+
+function Auth() {
+    return <div className="w-full sm:max-w-sm ">
+        <SendOTPForm />
+    </div>;
+}
+
+export default Auth;
