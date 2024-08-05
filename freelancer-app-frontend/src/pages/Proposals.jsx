5842e5b1c2a186b54e4db2822f7ad416ea459695
mobin <mbynatrzade@gmail.com>
2024-08-05 21:39:33 +0430

    added proposal table component

--- freelancer-app-frontend/src/pages/Proposals.jsx
+++ freelancer-app-frontend/src/pages/Proposals.jsx
@@ -1,6 +1,10 @@
+import ProposalTable from "../features/proposals/ProposalTable";
 
 function Proposals() {
-    return <div>Proposals</div>;
+    return <div>
+        <h1 className="font-black text-secondary-700 text-xl mb-8">پروپوزال های شما</h1>
+        <ProposalTable />
+    </div>;
 }
 
 export default Proposals;
