394f6276f87edefcc636c55df472a7e34a2ae3bd
mobin <mbynatrzade@gmail.com>
2024-08-06 16:53:43 +0430

    added filter dropdown component

--- freelancer-app-frontend/src/features/freelancer/project/ProjectHeader.jsx
+++ freelancer-app-frontend/src/features/freelancer/project/ProjectHeader.jsx
@@ -1,9 +1,21 @@
+import useCategories from "../../../hooks/useCategories";
+import FilterDropDown from "../../../ui/FilterDropDown";
 
 function ProjectHeader() {
+    const { transformedCategories } = useCategories();
+
     return (
         <div className="flex items-center justify-between text-secondary-700 mb-8 ">
             <h1 className="text-lg font-bold">لیست پروژه ها</h1>
-            <div>فیلتر ها</div>
+            <div>
+                <FilterDropDown
+                    filterField="category"
+                    options={[{
+                        value: "ALL",
+                        label: "دسته بندی (همه)"
+                    }, ...transformedCategories]}
+                />
+            </div>
         </div>
     )
 }
---
+++ freelancer-app-frontend/src/ui/Select.jsx
@@ -0,0 +1,12 @@
+
+function Select({ value, onChange, options }) {
+    return (
+        <select value={value} onChange={onChange} className="textField__input py-2 text-xs">
+            {options.map((item) => (
+                <option key={item.value} value={item.value}>{item.label}</option>
+            ))}
+        </select>
+    )
+}
+
+export default Select;
