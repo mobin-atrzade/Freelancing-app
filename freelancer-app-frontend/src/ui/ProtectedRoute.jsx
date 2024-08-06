7ee9e03904ef4f0662c7e9888e19fab63bb902d7
mobin <mbynatrzade@gmail.com>
2024-08-06 19:11:01 +0430

    added protected route

--- freelancer-app-frontend/src/ui/ProtectedRoute.jsx
+++ freelancer-app-frontend/src/ui/ProtectedRoute.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
 import useAuthorize from "../features/authentication/useAuthorize";
+import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
 
 function ProtectedRoute({ children }) {
+    const navigate = useNavigate();
     const { isAuthenticated, isLoading, isAuthorized } = useAuthorize();
-    console.log(isAuthorized);
-    return children;
+
+
+    useEffect(() => {
+        if (!isAuthenticated && !isLoading) navigate("/auth", { replace: true });
+        if (!isAuthorized && !isLoading) navigate("/not-access", { replace: true });
+    }, [isAuthenticated, isAuthorized, isLoading, navigate])
+
+
+    if (isLoading) return (
+        <div className="flex items-center justify-center h-screen bg-secondary-100">
+            <Loading />
+        </div>
+    )
+
+    if (isAuthenticated && isAuthorized) return children;
 }
 
 export default ProtectedRoute;
